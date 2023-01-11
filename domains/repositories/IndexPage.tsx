a54a0c218a734eaab5d9b5c5082d36da94641474
Norbert Witak <norbert.witak@olx.pl>
2023-01-11 10:34:02 +0100

    Improve Home page layout

--- domains/repositories/IndexPage.tsx
+++ domains/repositories/IndexPage.tsx
@@ -1,6 +1,7 @@
 import { SearchForm } from '@domains/repositories/components/SearchForm/SearchForm';
 import { createRepositoriesPathname } from '@domains/repositories/helpers/createRepositoriesPathname';
 import { Head } from '@domains/shared/components/Head/Head';
+import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
@@ -13,25 +14,27 @@ const IndexPage: FC = () => {
   return (
     <>
       <Head />
-      <StyledIndexPageContent>
-        <Grid container maxWidth="sm" rowSpacing={2}>
-          <Grid item xs={12}>
-            <SearchForm />
+      <Container>
+        <StyledIndexPageContent>
+          <Grid container maxWidth="sm" rowSpacing={2}>
+            <Grid item xs={12}>
+              <SearchForm />
+            </Grid>
+            <StyledCaptionGrid item xs={12}>
+              <Typography variant="caption">
+                Please enter a search term or try with{' '}
+                <NextLink
+                  legacyBehavior
+                  href={createRepositoriesPathname({ searchTerm: 'react', shouldEncodeSearchTerm: true })}
+                  passHref
+                >
+                  <Link>React</Link>
+                </NextLink>
+              </Typography>
+            </StyledCaptionGrid>
           </Grid>
-          <StyledCaptionGrid item xs={12}>
-            <Typography variant="caption">
-              Please enter a search term or try with{' '}
-              <NextLink
-                legacyBehavior
-                href={createRepositoriesPathname({ searchTerm: 'react', shouldEncodeSearchTerm: true })}
-                passHref
-              >
-                <Link>React</Link>
-              </NextLink>
-            </Typography>
-          </StyledCaptionGrid>
-        </Grid>
-      </StyledIndexPageContent>
+        </StyledIndexPageContent>
+      </Container>
     </>
   );
 };
